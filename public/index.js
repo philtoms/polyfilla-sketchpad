7e12e7c787aaf5b2fd5864f11ce5ab043ff51060
Phil Toms <phil.toms@hotmail.co.uk>
2020-03-22 10:20:48 +0000

    remove elements.js

--- public/index.js
+++ public/index.js
@@ -1,13 +1,15 @@
 import { createContext } from 'https://unpkg.com/hooked-elements?module';
 
 import log from './modules/utils/log.js';
-import elements from './modules/elements.js';
+import intro from './modules/elements/intro.js';
+import compose from './modules/elements/compose.js';
 
 function startup() {
   const context = createContext({
     state: 'intro'
   });
-  elements(context);
+  intro(context);
+  compose(context);
 }
 
 document.addEventListener('DOMContentLoaded', startup);
--- public/modules/elements.js
+++
@@ -1,7 +0,0 @@
-import intro from './elements/intro.js';
-import compose from './elements/compose.js';
-
-export default context => {
-  intro(context);
-  compose(context);
-};
