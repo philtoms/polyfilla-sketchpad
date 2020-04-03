044d0a8328ac3a22a1f42b194f6f64a930e9b4e2
Phil Toms <phil.toms@hotmail.co.uk>
2020-04-03 23:15:12 +0100

    exponential fade

--- public/modules/utils/touch.js
+++ public/modules/utils/touch.js
@@ -1,4 +1,5 @@
 const raf = requestAnimationFrame.bind(window) || setTimeout.bind(window);
+
 export const copy = (offsetX, offsetY) => (
   { identifier = 0, pageX, pageY, radiusX, radiusY, rotationAngle, force },
   paintType
@@ -10,35 +11,35 @@ export const copy = (offsetX, offsetY) => (
   radiusY,
   rotationAngle,
   force,
-  paintType
+  paintType,
 });
 
 export const fade = (ctx, width, height) => {
+  let o = 255;
   raf(() => {
     const imageData = ctx.getImageData(0, 0, width, height);
     const data = imageData.data;
     for (let i = 0; i < data.length; i += 4) {
-      data[i + 3] = Math.max(0, data[i + 3] - 5);
+      const opacity = data[i + 3];
+      const dfade = opacity - opacity / 250;
+      data[i + 3] = Math.max(0, dfade - 1);
     }
     ctx.putImageData(imageData, 0, 0);
     fade(ctx, width, height);
   }, 30);
 };
 
-let lastX, lastY;
-export const draw = ctx => ({ pageX, pageY, paintType }) => {
+export const draw = (ctx) => ({ pageX, pageY, paintType }) => {
   const color = `rgb(0,0,0)`;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+  ctx.lineWidth = 2;
   if (paintType === 'fill') {
     ctx.beginPath();
-    ctx.arc(pageX, pageY, 4, 0, 2 * Math.PI, false); // a circle at the start
-    ctx.fillStyle = color;
+    ctx.arc(pageX, pageY, 2, 0, 2 * Math.PI, false); // a circle at the start
     ctx.fill();
   } else {
     ctx.lineTo(pageX, pageY);
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = color;
     ctx.stroke();
   }
-  lastX = pageX;
-  lastY = pageY;
 };
