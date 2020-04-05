02002378b1b2f50c66495ab47d196d8e10b403d6
Phil Toms <phil.toms@hotmail.co.uk>
2020-04-05 12:53:01 +0100

    color-code

--- public/modules/elements/touch-backdrop.js
+++ public/modules/elements/touch-backdrop.js
@@ -1,4 +1,4 @@
-import { setRange, select, color } from '../melody/notes.js';
+import { setRange, scale, color, select } from '../melody/notes.js';
 
 const { define, render, useContext } = hookedElements;
 
@@ -10,30 +10,35 @@ export default (context) => {
     render() {
       const { state } = useContext(context);
       if (state === 'layout') {
-        const ctx = this.element.getContext('2d');
         const noteRange = setRange(
           this.element.parentElement.clientWidth,
           this.element.parentElement.clientHeight
         );
         const { range, span, orange, ospan } = noteRange;
+        this.element.parentElement.style.height = `${range * span}px`;
         this.element.height = range * span;
         this.element.width = orange * ospan;
-        for (let i = 0, j = span / 2; i < range; i++, j += span) {
-          ctx.beginPath();
-          ctx.moveTo(0, j);
-          ctx.lineTo(this.element.width, j);
-          ctx.lineWidth = span;
-          ctx.strokeStyle = `rgba(${color[select(0, j).key]},0.3)`;
-          ctx.stroke();
+
+        const ctx = this.element.getContext('2d');
+        ctx.beginPath();
+        for (let j = 0, idx = 0, x = 0; j < orange; j++, x += ospan) {
+          for (let i = 0, y = 0; i < range; i++, y += span, idx++) {
+            ctx.fillStyle = `rgba(${color[select(x + 1, y + 1).key]},0.5)`;
+            // ctx.fillStyle = `rgba(10,20,30,${(1 + (idx % scale.length)) / 20})`;
+            // ctx.fillStyle = `rgba(${color[select(x + 1, y + 1).key]},${
+            //   (1 + (idx % scale.length)) / 10
+            // })`;
+            ctx.fillRect(x, y, ospan, span);
+          }
         }
-        for (let i = 1, j = ospan + ospan / 2; i < orange; i++, j += ospan) {
-          ctx.beginPath();
-          ctx.moveTo(j + 1, 0);
-          ctx.lineTo(j + 1, this.element.height);
-          ctx.lineWidth = ospan;
-          ctx.strokeStyle = `rgba(0,0,0,${i / 10})`;
-          ctx.stroke();
-        }
+
+        // for (let i = 1, j = ospan + ospan / 2; i < orange; i++, j += ospan) {
+        //   ctx.beginPath();
+        //   ctx.lineTo(j + 1, this.element.height);
+        //   ctx.lineWidth = ospan;
+        //   ctx.strokeStyle = `rgba(0,0,0,${i / 10})`;
+        //   ctx.stroke();
+        // }
         context.provide({ ...context.value, state: 'compose', noteRange });
       }
     },
